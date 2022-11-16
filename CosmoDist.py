d736cb06a45134f12d80b78e774d81617205f4aa
Joachim Harnois-Deraps <joachim.harnois-deraps@ncl.ac.uk>
2022-11-16 10:02:56 +0000

    Remove unnecessary python libraries in CosmoDist.py, which makes every call much faster.

--- CosmoDist.py
+++ CosmoDist.py
@@ -1,14 +1,9 @@
-from astropy import cosmology
 from astropy.cosmology import w0waCDM
-import astropy.units as u
 from astropy.cosmology import z_at_value
-import numpy as np
+from numpy import savetxt, loadtxt
 
 #print("*** USING PYTHON COSMODIST ***")
-cosmo_par_array = np.loadtxt('cosmo_par.tmp', usecols=(0,1,2,3,4), unpack=True)
-#file = np.loadtxt("./snapshots.dat",comments='%#',usecols=(0,4,5,6,7,8),unpack=True)
-
-#print(cosmo_par_array)
+cosmo_par_array = loadtxt('cosmo_par.tmp', usecols=(0,1,2,3,4), unpack=True)
 
 Om_m = cosmo_par_array[0]
 Om_l = cosmo_par_array[1]
@@ -16,7 +11,6 @@ w_de = cosmo_par_array[2]
 h_0 = cosmo_par_array[3]
 z = cosmo_par_array[4]
 
-#cosmo = w0waCDM(H0=68.98, Om0=0.2905, Ode0=0.7095, w0=-1.0, wa=0.0)
 cosmo = w0waCDM(H0=h_0*100.0, Om0=Om_m, Ode0=Om_l, w0=w_de, wa=0.0)
 
 #print("***")
@@ -24,31 +18,12 @@ cosmo = w0waCDM(H0=h_0*100.0, Om0=Om_m, Ode0=Om_l, w0=w_de, wa=0.0)
 #print(cosmo)
 #print("***")
 
-#-------------------
-# Example
-#Set the width of boxes to collapse:
-#Box_Mpc_over_h = (505.0/2.0)*u.Mpc
-#Box = Box_Mpc_over_h / cosmo.h # in Mpc
-
-#print("Box size:")
-#print(Box*cosmo.h)
-#print(Box_Mpc_over_h)
-#print("***")
-
-#z_test = z_at_value(cosmo.comoving_distance, Box)
-#chi_test = cosmo.comoving_distance(z_test)
-#chi_test_Mpc_over_h = chi_test*cosmo.h
-#print("comoving distance to the first redshift plane:")
-#print(chi_test_Mpc_over_h)
-#print("***")
-#----------------
-
 chi_Mpc_over_h = cosmo.comoving_distance(z)*cosmo.h
 #print(chi_Mpc_over_h)
+
 #print("*** DONE PYTHON COSMODIST ***")
 
 file_out_str= "./chi_from_python.tmp"
 data = [chi_Mpc_over_h.value, Om_m, Om_l, w_de, h_0, z]
 #print('data:',data)
-np.savetxt(file_out_str,data, fmt='%.8e')
-
+savetxt(file_out_str,data, fmt='%.8e')
